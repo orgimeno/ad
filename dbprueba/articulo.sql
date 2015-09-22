bd6abfd825142c7f31b336ac048a1a9507621748
orgimeno <orgimeno@gmail.com>
2015-09-22 08:52:06 +0200

    añadir articulos y categorias

---
+++ dbprueba/articulo.sql
@@ -0,0 +1,9 @@
+insert into articulo (nombre, categoria, precio) values ('articulo 1',1,1);
+
+insert into articulo (nombre, categoria, precio) values ('articulo 2',2,2);
+
+insert into articulo (nombre, categoria, precio) values ('articulo 3',3,3.5);
+
+insert into articulo (nombre, categoria ) values ('articulo 4',2);
+
+insert into articulo (nombre, categoria) values ('articulo 5',5);
---
+++ dbprueba/categoria.sql
@@ -0,0 +1,7 @@
+insert into categoria (nombre) values ('categoria 1');
+
+insert into categoria (nombre) values ('categoria 2');
+
+insert into categoria (nombre) values ('categoria 3');
+
+
