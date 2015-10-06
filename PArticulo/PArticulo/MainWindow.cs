de0fb3a67099dda27c125545e9a0895aca178b12
orgimeno <orgimeno@gmail.com>
2015-10-06 09:28:35 +0200

    Mostrar mas de dos columnas

--- PArticulo/PArticulo/MainWindow.cs
+++ PArticulo/PArticulo/MainWindow.cs
@@ -1,38 +1,58 @@
 using System;
 using Gtk;
 using MySql.Data.MySqlClient;
+
 public partial class MainWindow: Gtk.Window
 {	
 	public MainWindow (): base (Gtk.WindowType.Toplevel)
 	{
 		Build ();
 
-		Console.Write ("hola mundo");
-		/*
-		MySqlConnection mySqlConnection = new MySqlConnection ("Database=dbprueba;Data Source=localhost;User id=root;Password=sistemas");
-		mySqlConnection.Open();
+		Console.WriteLine ("MainWindow ctor.");
+		MySqlConnection mySqlConnection = new MySqlConnection (
+			"Database=dbprueba;Data Source=localhost;User Id=root;Password=sistemas"
+		);
+		mySqlConnection.Open ();
 
 		MySqlCommand mySqlCommand = mySqlConnection.CreateCommand ();
-
-		mySqlCommand.CommandText="select id, nombre from articulo";
+		mySqlCommand.CommandText = "select * from articulo";
 
 		MySqlDataReader mySqlDataReader = mySqlCommand.ExecuteReader ();
 
+		ListStore listStore = new ListStore (typeof(String), typeof(String), typeof(String), typeof(String));
+
 		while (mySqlDataReader.Read()) {
-			Console.WriteLine ("id={0} nombre={1}", mySqlDataReader[0], mySqlDataReader[1]);
+			Console.WriteLine ("id={0} nombre={1}", mySqlDataReader [0], mySqlDataReader [1]);
+			listStore.AppendValues (mySqlDataReader [0].ToString(), mySqlDataReader [1], mySqlDataReader [2].ToString(), mySqlDataReader[3].ToString());
 		}
+
 
 		mySqlDataReader.Close ();
 		mySqlConnection.Close ();
-		*/
-		treeView.AppendColumn ("id", new CellRendererText(),"text",0);
-		treeView.AppendColumn ("nombre", new CellRendererText(),"text",1);
-		ListStore liststore = new ListStore (typeof(long), typeof(String));
 
-		liststore.AppendValues ("1l", "nombre del primero");
-		treeView.Model = liststore;
-		liststore.AppendValues ("2l", "nombre del segundo");
-	}	
+		//añado columnas
+		treeView.AppendColumn ("id", new CellRendererText (), "text", 0);
+		treeView.AppendColumn ("nombre", new CellRendererText (), "text", 1);
+		treeView.AppendColumn ("categoria", new CellRendererText (), "text", 2);
+		treeView.AppendColumn ("precio", new CellRendererText (), "text", 3);
+
+		//establezco el modelo
+		ListStore listStore1 = new ListStore (typeof(String), typeof(String));
+		//TODO rellenar listStore
+
+
+		listStore.AppendValues ("1", "Nombre del primero");
+
+		treeView.Model = listStore;
+
+		string[] values = new string[2];
+		values [0] = "2";
+		values [1] = "Nombre del segundo";
+		listStore.AppendValues (values);
+
+
+
+	}
 
 	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
 	{
