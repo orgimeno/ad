d5ab199ae9fc4bc458ab405246b7d04361e97d72
orgimeno <orgimeno@gmail.com>
2015-12-17 10:44:04 +0100

    Modular select y connection

--- GMySql/src/main/java/org/institutoserpis/ad/PruebaArticulo.java
+++ GMySql/src/main/java/org/institutoserpis/ad/PruebaArticulo.java
@@ -12,27 +12,7 @@ public class PruebaArticulo {
 	public static void main(String[] args) throws SQLException {
 		
 		Connection connection=connectar();
-		String query = "SELECT * FROM articulo";
-		 
-		  // create the java statement
-		  Statement st = (Statement) connection.createStatement();
-		   
-		  // execute the query, and get a java resultset
-		  ResultSet rs = st.executeQuery(query);
-		   
-		  // iterate through the java resultset
-		  while (rs.next())
-		  {
-		    int id = rs.getInt("id");
-		    String name = rs.getString("nombre");
-		    Double precio = rs.getDouble("precio");
-		    //String lastName = rs.getString("last_name");
-		    //int numPoints = rs.getInt("num_points");
-		     
-		    // print the results
-		    System.out.format("%s, %s, %s \n", id, name, precio);
-		  }
-		
+		doSelect(connection);
 		connection.close();
 		System.out.println("fin");
 }
@@ -42,6 +22,24 @@ public class PruebaArticulo {
 				"jdbc:mysql://localhost/dbprueba", "root", "sistemas");
 		return connection;
 		
+	}
+	
+	public static void doSelect(Connection connection) throws SQLException{
+		String query = "SELECT * FROM articulo";
+		//create the java statement
+		Statement st = (Statement) connection.createStatement();
+		// 	execute the query, and get a java resultset
+		ResultSet rs = st.executeQuery(query);
+		// iterate through the java resultset
+		while (rs.next())
+		{
+			int id = rs.getInt("id");
+		    String name = rs.getString("nombre");
+		    Double precio = rs.getDouble("precio");
+		     
+		    // print the results
+		    System.out.format("%s, %s, %s \n", id, name, precio);
+		  }
 	}
 
 }
