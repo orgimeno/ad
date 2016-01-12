4f56457e480e238536e16ba113a85e897f3ab649
orgimeno <orgimeno@gmail.com>
2016-01-12 10:36:11 +0100

    editItem

--- GMySql/src/main/java/org/institutoserpis/ad/PruebaArticulo.java
+++ GMySql/src/main/java/org/institutoserpis/ad/PruebaArticulo.java
@@ -6,6 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Scanner;
 
+import com.mysql.jdbc.PreparedStatement;
 import com.mysql.jdbc.Statement;
 
 public class PruebaArticulo {
@@ -13,13 +14,14 @@ public class PruebaArticulo {
 	private static Scanner tec;
 
 	public static void main(String[] args) throws SQLException {
-		
+		int opt=1;
 		tec = new Scanner(System.in);
 		Connection connection=connectar();
-		
-		showMenu();
-		selectMenu(tec.nextInt(),connection, tec);
-		
+		do{
+			showMenu();
+			opt=tec.nextInt();
+			selectMenu(opt,connection, tec);
+		}while(opt!=0);
 		connection.close();
 		System.out.println("fin");
 }
@@ -104,13 +106,60 @@ public class PruebaArticulo {
 		}
 	}
 	
+	public static void editItem(Scanner tec, Connection connection) throws SQLException{
+		System.out.println("Introduzca la id del registro a modificar");
+		int id= tec.nextInt();
+
+		System.out.println("Introduzca el nuevo nombre");
+		String nombre= tec.next();
+
+		System.out.println("Introduzca el nuevo precio");
+		float precio = tec.nextFloat();
+		
+		String query = "SELECT * FROM categoria";
+		//create the java statement
+		Statement st = (Statement) connection.createStatement();
+		// 	execute the query, and get a java resultset
+		ResultSet rs = st.executeQuery(query);
+		// iterate through the java resultset
+		while (rs.next())
+		{
+		    String name = rs.getString("nombre");
+		    System.out.format("Categoria: "+ name+"\n");
+		 }
+		System.out.println("Selecione categoria nueva");
+		int categoria= tec.nextInt();
+		
+		  try
+		  {
+		    // create our java preparedstatement using a sql update query
+		    String queryUpdate=
+		      "UPDATE Articulo SET nombre = ?, precio = ?, categoria = ?, WHERE id = ?;";
+		    PreparedStatement ps = (PreparedStatement) connection.prepareStatement(queryUpdate);
+		    // set the preparedstatement parameters
+		    ps.setString(1,nombre);
+		    ps.setFloat(2,precio);
+		    ps.setInt(3,categoria);
+		    ps.setInt(4,id);
+		 
+		    // call executeUpdate to execute our sql update statement
+		    ps.executeUpdate();
+		    ps.close();
+		  }
+		  catch (SQLException sqlE)
+		  {
+		    // log the exception
+			  System.err.println(sqlE.getMessage());
+		  }
+	}
+	
 	public static void selectMenu(int op, Connection connection, Scanner tec) throws SQLException{
 		
 		switch(op){
 		case 0:break;
 		case 1:selectOne(tec, connection);break;
 		case 2:newRegistry(tec, connection);break;
-		case 3:;break;
+		case 3:editItem(tec, connection);break;
 		case 4:;break;
 		case 5:doSelect(connection);
 		break;
