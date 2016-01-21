e475e1370ea7c57740d821f840ca76eeabcf2223
orgimeno <orgimeno@gmail.com>
2016-01-21 10:39:42 +0100

    find articulo and delete articulo with Hibernate

--- GHibernate/src/main/java/org/institutoserpis/ad/PruebaArticulo.java
+++ GHibernate/src/main/java/org/institutoserpis/ad/PruebaArticulo.java
@@ -1,13 +1,15 @@
 package org.institutoserpis.ad;
 
 import java.util.List;
+import java.util.Scanner;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class PruebaArticulo {
-
+	private static Scanner scanner = new Scanner(System.in);
+	
 	public static void main(String[] args) {
 		System.out.println("inicio");
 		EntityManagerFactory entityManagerFactory = 
@@ -15,18 +17,58 @@ public class PruebaArticulo {
 		
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
-		List<Articulo> articulos = entityManager.createQuery("from Articulo", Articulo.class).getResultList();
-		for (Articulo articulo : articulos)
-			System.out.printf("%5s %-30s %5s %10s\n", 
-					articulo.getId(), 
-					articulo.getNombre(), 
-					articulo.getCategoria(), 
-					articulo.getPrecio()
-			);
+
+		listarArticulos(entityManager);
+		buscarArticulo(entityManager);
+		eliminarArticulo(entityManager);
 		entityManager.getTransaction().commit();
 		entityManager.close();
 		
 		entityManagerFactory.close();
+	}
+	
+	private static long scanLong(String label) {
+		while (true) {
+			System.out.print(label);
+			String data = scanner.nextLine().trim();
+			try {
+				return Long.parseLong(data);
+			} catch (NumberFormatException ex) {
+				System.out.println("Debe ser un número");
+			}
+		}
+	}	
+	private static String scanString(String label) {
+		System.out.print(label);
+		return scanner.nextLine().trim();
+	}
+	
+	private static void showData(Articulo articulo){
+		System.out.printf("%5s %-30s %5s %10s\n", 
+				articulo.getId(), 
+				articulo.getNombre(), 
+				articulo.getCategoria(), 
+				articulo.getPrecio()
+		);
+	}
+	
+	public static void listarArticulos(EntityManager entityManager){
+		List<Articulo> articulos = entityManager.createQuery("from Articulo", Articulo.class).getResultList();
+		for (Articulo articulo : articulos)
+			showData(articulo);
+	}
+	
+	public static void buscarArticulo(EntityManager entityManager){
+		long id = scanLong("Introduzca una id para buscar");
+		Articulo articulo = entityManager.find(Articulo.class, id);
+		showData(articulo);
+
+	}
+	
+	public static void eliminarArticulo(EntityManager entityManager){
+		long id = scanLong("Introduzca una id para eliminar");
+		Articulo articulo = entityManager.find(Articulo.class, id);
+		entityManager.remove(articulo);
 	}
 
 }
