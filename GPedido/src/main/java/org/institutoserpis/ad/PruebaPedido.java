4f07b2f5852482e00173c3f653068e4c43889777
orgimeno <orgimeno@gmail.com>
2016-02-09 09:46:22 +0100

    Gpedido con dependencias añadidas mediante grade

---
+++ GPedido/src/main/java/org/institutoserpis/ad/PruebaPedido.java
@@ -0,0 +1,9 @@
+package org.institutoserpis.ad;
+
+public class PruebaPedido {
+	
+	public static void main (String[] args){
+		
+	}
+
+}
--- dbprueba.sql
+++ dbprueba.sql
@@ -9,3 +9,24 @@ CREATE TABLE IF NOT EXISTS `categoria` (
   `id` bigint auto_increment primary key,
   `nombre` varchar(50) NOT NULL UNIQUE
 );
+
+CREATE TABLE IF NOT EXISTS `cliente` (
+  `id` bigint auto_increment primary key,
+  `nombre` varchar(50) NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS `pedido` (
+  `id` bigint auto_increment primary key,
+  `cliente` bigint not null,
+  `fecha` datetime not null,
+  `importe` decimal(10,2)
+);
+
+CREATE TABLE IF NOT EXISTS `pedidolinea` (
+  `id` bigint auto_increment primary key,
+  `pedido` bigint not null,
+  `articulo` bigint not null,
+  `precio` decimal(10,2),
+  `unidades` decimal(10,2)
+  `importe` decimal(10,2),
+);
